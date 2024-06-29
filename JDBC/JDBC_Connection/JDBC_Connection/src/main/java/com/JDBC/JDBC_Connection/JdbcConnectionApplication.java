cdc6ec6ce46950365ba20159a5a5adfbcec7c881
naveendesilva <naveen.desilva@Directfn.com>
2024-06-29 08:36:03 +0530

    Initial JDBC Commit for Spring Boot

---
+++ JDBC/JDBC_Connection/JDBC_Connection/src/main/java/com/JDBC/JDBC_Connection/JdbcConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.JDBC.JDBC_Connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcConnectionApplication.class, args);
+	}
+
+}
