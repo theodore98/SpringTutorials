cb95d22ec76440c33bff3453e0d3e6228280e6c0
naveendesilva <naveen.desilva@Directfn.com>
2024-06-30 13:21:26 +0530

    Establishing Database Connection to h2 through JDBC Template. Usage of JDBC Template Features for addition and retrieval queries

--- JDBC/JDBC_Connection/JDBC_Connection/src/main/java/com/JDBC/JDBC_Connection/JdbcConnectionApplication.java
+++ JDBC/JDBC_Connection/JDBC_Connection/src/main/java/com/JDBC/JDBC_Connection/JdbcConnectionApplication.java
@@ -1,13 +1,32 @@
 package com.JDBC.JDBC_Connection;
 
+import com.JDBC.JDBC_Connection.model.Alien;
+import com.JDBC.JDBC_Connection.repository.AlienRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class JdbcConnectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JdbcConnectionApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(JdbcConnectionApplication.class, args);
+		System.out.println("Naveen");
+
+		Alien a = context.getBean(Alien.class);
+		a.setId(100);
+		a.setName("Naveen");
+		a.setTech("Java");
+
+		AlienRepository repository = context.getBean(AlienRepository.class);
+		repository.save(a);
+
+		System.out.println(repository.getAllAliens());
+
+
+
 	}
 
 }
